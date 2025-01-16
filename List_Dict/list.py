dd5985d9e225aa5a49e9752632c84a83fb45354f
Michel Nzikou Mamboukou <00110138@uwa.edu.au>
2025-01-16 18:12:16 +0800

    Symbol and float vs integer

---
+++ List_Dict/list.py
@@ -0,0 +1,38 @@
+# some symbols
+# 1- != it means NOT EQUAL, == is EQUAL
+
+a= int(input(" Enter a number: "))
+
+if a==2:
+	print('I am right, Yeah')
+else:
+	print('I am wrong')
+
+## 1 is an integer
+## 1.0 is a float
+
+#dd=type(1)    # <class 'int'>
+## Formating and printing
+dd2= type(1.0)  # <class 'float'>
+dd= [a for a in range(a)]
+print(dd)
+print(f"What is y list dd: {dd} "+str(dd))
+print(f"What is the type of {dd2}")
+pi=3.1416  # this is PI
+b=float(a*10)*pi
+#  '1' or "1" are string
+print(f"is this (1 is 1.0) true or false? {1 is 1.0}")
+print(f" Is this operation possible? {1*b} and {b**2}")
+# > greater, >= greater than equal
+# < less than, <= less than equal
+# % formating, 
+
+## Float Numbers
+print(f" What is (1+1.0) = {1+1.0}")
+print(f" What is {a} + {b} = {a+b}")
+print(f" What is {a} / {b} = {a/b}")
+print(f" What is {a} * {b} = {a*b}")
+print(f" let round off {round(a/b,5)}")
+
+
+
