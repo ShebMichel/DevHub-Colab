a19e55b399030a9fbdbdfe5504e651ecfff3d8fc
Michel Nzikou Mamboukou <00110138@uwa.edu.au>
2025-01-25 14:50:40 +0800

    added roll dice with list of results

---
+++ KidsZone/roll_dice_with_results_in_a_dictionary.py
@@ -0,0 +1,67 @@
+
+import random
+import time
+
+# Create an empty dictionary
+empty_dict={}
+# to add something in a list we use [].append(a)
+for i in range(5):
+    my_dice_rol = random.randint(1, 6)
+    # create a key variable for your dictionary
+    dice_rol='throw number '+str(i+1)
+    empty_dict[dice_rol]=my_dice_rol
+    print(f"Give me the dict every time I roll the dice {empty_dict}")
+    time.sleep(6)
+    #return empty_list
+print(f"What is the the final dictionary {empty_dict}")
+
+#dice_list = function_to_give_dice_results()
+#print(f"What is the the final list {dice_list}")
+import random
+import time
+
+# Create an empty dictionary
+empty_dict={}
+# to add something in a list we use [].append(a)
+for i in range(5):
+    my_dice_rol = random.randint(1, 6)
+    # create a key variable for your dictionary
+    dice_rol='throw number '+str(i+1)
+    empty_dict[dice_rol]=my_dice_rol
+    print(f"Give me the dict every time I roll the dice {empty_dict}")
+    time.sleep(6)
+    #return empty_list
+print(f"What is the the final dictionary {empty_dict}")
+
+#dice_list = function_to_give_dice_results()
+#print(f"What is the the final list {dice_list}")
+# import random
+import time
+
+# Create an empty dictionary
+empty_dict={}
+# to add something in a list we use [].append(a)
+for i in range(5):
+    my_dice_rol = random.randint(1, 6)
+    # create a key variable for your dictionary
+    dice_rol='throw number '+str(i+1)
+    empty_dict[dice_rol]=my_dice_rol
+    print(f"Give me the dict every time I roll the dice {empty_dict}")
+    time.sleep(6)
+    #return empty_list
+print(f"What is the the final dictionary {empty_dict}")
+
+
+# A1={a:10, b:20,.......}
+# B1={b:20,.......,a:10}
+# A1[a]=10
+# B1[a]=10
+# #
+
+# AA=[a,b]
+# BB=[b,a]
+# AA[0]=a
+# BB[0]=b
+# # AA!=BB
+# ##
+
